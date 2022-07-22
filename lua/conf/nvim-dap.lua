5f53f6ca420dc717d6e7fea3501e45ca3baa155e
xYx <1yif@sina.com>
2022-07-22 17:35:26 +0800

    0721

--- lua/conf/nvim-dap.lua
+++ lua/conf/nvim-dap.lua
@@ -7,11 +7,6 @@ local dap = require("dap")
 vim.fn.sign_define("DapBreakpoint", {text = "●", texthl = "TodoFgFIX", linehl = "", numhl = ""})
 -- 加载调试器配置
 local dap_config = {
-<<<<<<< HEAD
-=======
-    rust = require("dap.rust"),
-    cpp = require("dap.rust"),
->>>>>>> 7d6c33b5fe6e15e9d088b9b91bae393a473d21a4
     java = require("dap.java"),
     -- rust = require("dap.lldb"),
     -- cpp = require("dap.lldb"),
@@ -21,15 +16,7 @@ local dap_config = {
 }
 -- 设置调试器
 for dap_name, dap_options in pairs(dap_config) do
-<<<<<<< HEAD
     dap.adapters = dap_options.adapters
-=======
-    if dap_name == 'rust' or dap_name == 'c' or dap_name == 'cc' or dap_name == 'cpp' then
-        dap.adapters = dap_options.adapters;
-    else
-        dap.adapters[dap_name] = dap_options.adapters
-    end
->>>>>>> 7d6c33b5fe6e15e9d088b9b91bae393a473d21a4
     dap.configurations[dap_name] = dap_options.configurations
 end
 
