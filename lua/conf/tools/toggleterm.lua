1e909447998b556b1fc4332aeca82e063f62fc44
xYx <1yif@sina.com>
2022-09-23 17:21:48 +0800

    ..

--- lua/conf/tools/toggleterm.lua
+++ lua/conf/tools/toggleterm.lua
@@ -11,6 +11,11 @@
 -- sudo apt-get update
 -- sudo apt-get install lazygit
 
+local function inInsert()
+    -- 删除 Esc 的映射
+    vim.keybinds.dgmap("t", "<Esc>")
+end
+
 local Toggleterm = require("toggleterm")
 Toggleterm.setup(
     {
@@ -19,20 +24,24 @@ Toggleterm.setup(
         -- 设置终端打开的大小
         size = 10,
         -- 打开普通终端时，关闭拼写检查
-        on_open = function()
+        on_open = function(term)
             vim.cmd("setlocal nospell")
+            inInsert()
+            vim.keybinds.bmap(term.bufnr, "t", "<C-t>", "<C-\\><C-n>:close<CR>", vim.keybinds.opts)
+            vim.keybinds.bmap(term.bufnr, "t", "<C-w>", "<C-\\><C-n>", vim.keybinds.opts)
+        end,
+        on_close = function()
+            -- 重新映射 Esc
+            vim.keybinds.gmap("t", "<Esc>", "<C-\\><C-n>", vim.keybinds.opts)
         end
+
     }
 )
 -- 新建终端
 local Terminal = require("toggleterm.terminal").Terminal
-local function inInsert()
-    -- 删除 Esc 的映射
-    vim.keybinds.dgmap("t", "<Esc>")
-end
 -- 新建浮动终端
 local floatTerm =
-    Terminal:new(
+Terminal:new(
     {
         hidden = true,
         direction = "float",
@@ -52,7 +61,7 @@ local floatTerm =
 )
 -- 新建 lazygit 终端
 local lazyGit =
-    Terminal:new(
+Terminal:new(
     {
         cmd = "lazygit",
         hidden = true,
