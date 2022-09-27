d63e6273e6c34b6906b6eff202744cfe18a71cab
xYx <1yif@sina.com>
2022-09-27 09:48:20 +0800

    ..

--- snippets/luasnip/java.lua
+++ snippets/luasnip/java.lua
@@ -19,43 +19,63 @@ local types = require("luasnip.util.types")
 local conds = require("luasnip.extras.expand_conditions")
 
 -- local utils = require("luasnip_snippets.utils")
-
-local package_str = "package " .. string.gsub(string.match(vim.fn.expand('%'), "/src/main/java/(.*)/.*java"), "/", ".") .. ";"
+local function package_str()
+    return  "package " .. string.gsub(string.match(vim.fn.expand('%'), "/src/main/java/(.*)/.*java"), "/", ".") .. ";"
+end
+-- local package_str = "package " .. string.gsub(string.match(vim.fn.expand('%'), "/src/main/java/(.*)/.*java"), "/", ".") .. ";"
+-- local function file_base_name()
+--     return vim.fn.expand('%:t:r')
+-- end
 local file_base_name = vim.fn.expand('%:t:r')
 
 return {
     s("clas", {
-        t(package_str),
+        f(function (_, snip)
+            return "package " .. string.gsub(string.match(snip.env.TM_FILEPATH, "/src/main/java/(.*)/.*java"), "/", ".") .. ";"
+        end, {}),
         t({ "", "", "" }),
         t({ "/**", "" }),
         t({ "* @author: xYx", "" }),
         t({ "* @date: " .. os.date("%Y-%m-%d %H:%M:%S"), "" }),
         t({ "*/", "" }),
-        t({ "public class " .. file_base_name .. " {" }),
+        -- t({ "public class " .. file_base_name .. " {" }),
+        f(function (_, snip)
+            return "public class " .. snip.env.TM_FILENAME_BASE .. " {"
+        end, {}),
         t({ "", "\t" }),
         i(0),
         t({ "", "}" }),
     }),
     s("iface", {
-        t(package_str),
+        f(function (_, snip)
+            return "package " .. string.gsub(string.match(snip.env.TM_FILEPATH, "/src/main/java/(.*)/.*java"), "/", ".") .. ";"
+        end, {}),
         t({ "", "", "" }),
         t({ "/**", "" }),
         t({ "* @author: xYx", "" }),
         t({ "* @date: " .. os.date("%Y-%m-%d %H:%M:%S"), "" }),
         t({ "*/", "" }),
-        t({ "public interface " .. file_base_name .. " {" }),
+        -- t({ "public interface " .. file_base_name .. " {" }),
+        f(function (_, snip)
+            return "public interface " .. snip.env.TM_FILENAME_BASE .. " {"
+        end, {}),
         t({ "", "\t" }),
         i(0),
         t({ "", "}" }),
     }),
     s("enu", {
-        t(package_str),
+        f(function (_, snip)
+            return "package " .. string.gsub(string.match(snip.env.TM_FILEPATH, "/src/main/java/(.*)/.*java"), "/", ".") .. ";"
+        end, {}),
         t({ "", "", "" }),
         t({ "/**", "" }),
         t({ "* @author: xYx", "" }),
         t({ "* @date: " .. os.date("%Y-%m-%d %H:%M:%S"), "" }),
         t({ "*/", "" }),
-        t({ "public enum " .. file_base_name .. " {" }),
+        -- t({ "public enum " .. file_base_name .. " {" }),
+        f(function (_, snip)
+            return "public enum " .. snip.env.TM_FILENAME_BASE .. " {"
+        end, {}),
         t({ "", "\t" }),
         i(0),
         t({ "", "}" }),
