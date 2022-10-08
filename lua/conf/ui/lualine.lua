a447e3af62a7376066e3aa9df8ab27d694008390
yil3 <1yif@sina.com>
2022-10-08 23:16:15 +0800

    ..

--- lua/conf/ui/lualine.lua
+++ lua/conf/ui/lualine.lua
@@ -9,7 +9,23 @@ require('lualine').setup {
     sections = {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { 'filename', 'lsp_progress' },
+        lualine_c = {
+            'filename',
+            {
+                'lsp_progress',
+                -- separators = {
+                --     component = ' ',
+                --     progress = ' | ',
+                --     percentage = { pre = '', post = '%% ' },
+                --     title = { pre = '', post = ': ' },
+                --     lsp_client_name = { pre = '[', post = ']' },
+                --     spinner = { pre = '', post = '' },
+                --     message = { pre = '(', post = ')', commenced = 'In Progress', completed = 'Completed' },
+                -- },
+                -- display_components = { 'lsp_client_name', 'spinner', { 'title', 'percentage', 'message' } },
+                -- timer = { progress_enddelay = 500, spinner = 1000, lsp_client_name_enddelay = 1000 },
+            }
+        },
         lualine_x = { 'encoding', 'filetype' },
         lualine_y = { 'progress' },
         lualine_z = { 'location' }
