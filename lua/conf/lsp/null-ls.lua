c324f90ecc43caf49155b1b7a8ef558e011d92fc
xYx <1yif@sina.com>
2022-10-27 17:54:06 +0800

    ..

--- lua/conf/lsp/null-ls.lua
+++ lua/conf/lsp/null-ls.lua
@@ -1,7 +1,21 @@
 local null_ls = require("null-ls")
 null_ls.setup({
     sources = {
-        null_ls.builtins.formatting.prettierd,
+        null_ls.builtins.formatting.prettierd.with({
+            env = {
+                PRETTIERD_DEFAULT_CONFIG = {
+                    printWidth = 120,
+                    singleQuote = true,
+                    trailingComma = "all",
+                    arrowParens = "avoid",
+                    bracketSpacing = true,
+                    endOfLine = "auto",
+                    semi = true,
+                    tabWidth = 2,
+                    useTabs = false,
+                }
+            }
+        }),
         null_ls.builtins.formatting.sql_formatter,
     },
 })
