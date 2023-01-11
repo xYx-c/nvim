1bd06517fb3f1f9d1cb79808317b0809d851e48e
xYx <1yif@sina.com>
2023-01-11 11:22:05 +0800

    ..

--- lua/conf/tools/ChatGPT.lua
+++ lua/conf/tools/ChatGPT.lua
@@ -1,6 +1,5 @@
 -- https://github.com/jackMort/ChatGPT.nvim
 
-vim.env.OPENAI_API_KEY = "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"
 require("chatgpt").setup({
     max_line_length = 0,
 })
