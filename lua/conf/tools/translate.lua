eeda0a06bc03ba227d5959151003e6d3e4f07fed
x <xyx.alex.c@gmail.com>
2023-03-03 14:43:43 +0800

    ..

--- lua/conf/tools/translate.lua
+++ lua/conf/tools/translate.lua
@@ -7,6 +7,15 @@
 -- $ make
 -- $ [sudo] make install
 
+local proxy = os.execute("pgrep -x clash > /dev/null");
+local function args()
+    if proxy then
+        return { "-x", "http://127.0.0.1:7890" }
+    -- else
+        -- return { "-e", "bing" }
+    end
+end
+
 local options = {
     default = {
         command = "translate_shell",
@@ -23,11 +32,11 @@ local options = {
                 zindex = 9999, -- a large number
             },
         },
-        -- command = {
-        --     translate_shell = {
-        --         args = { "-e", "bing" },
-        --     },
-        -- }
+        command = {
+            translate_shell = {
+                args = args(),
+            },
+        }
     }
 }
 
