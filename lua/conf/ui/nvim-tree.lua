320f4736c73e7ce4157822621832b50dfd6cb0cd
xYx <xYx.c@outlook.com>
2023-05-08 16:41:33 +0800

    ..

--- lua/conf/ui/nvim-tree.lua
+++ lua/conf/ui/nvim-tree.lua
@@ -6,8 +6,18 @@ vim.g.loaded_netrwPlugin = 1
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
+local function on_attach(bufnr)
+    local api = require('nvim-tree.api')
+    local function opts(desc)
+        return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
+    api.config.mappings.default_on_attach(bufnr)
+    vim.keymap.set('n', '-', api.tree.change_root_to_parent, opts('Up'))
+    vim.keymap.set('n', '+', api.tree.change_root_to_node, opts('CD'))
+end
 
 require("nvim-tree").setup({
+    on_attach = on_attach,
     -- 视图
     view = {
         adaptive_size = true,
