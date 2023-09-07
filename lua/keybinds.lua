eb59942f155e22f61aae639f6653e40f86c022a3
x <xYx.c@outlook.com>
2023-09-07 17:12:02 +0800

    ...

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -61,8 +61,8 @@ vim.keybinds.gmap("n", "<C-b>", "<cmd>TodoTelescope theme=dropdown<CR>", vim.key
 -- codeium AI提示
 vim.g.codeium_disable_bindings = 1
 vim.keymap.set('i', '<c-l>', function () return vim.fn['codeium#Accept']() end, { expr = true })
-vim.keymap.set('i', '<c-]>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
-vim.keymap.set('i', '<c-p>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
+vim.keymap.set('i', '<m-]>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+vim.keymap.set('i', '<m-[>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
 vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
 ---------------------------------------------------------------------------
 -- lsp
