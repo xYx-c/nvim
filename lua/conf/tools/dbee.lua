317605f8c8edb810b62f78394c6b79244f6dfccc
x <xYx.c@outlook.com>
2024-07-23 11:37:11 +0800

    ..

--- lua/conf/tools/dbee.lua
+++ lua/conf/tools/dbee.lua
@@ -2,13 +2,13 @@
 
 require("dbee").setup({
     sources = {
-        require("dbee.sources").MemorySource:new({
-            -- {
-            --     name = "postgres",
-            --     type = "postgres",
-            --     url = "postgres://root:root@172.18.0.10/postgres?sslmode=disable",
-            -- },
-        }),
+        -- require("dbee.sources").MemorySource:new({
+        --     {
+        --         name = "postgres",
+        --         type = "postgres",
+        --         url = "postgres://root:root@172.18.0.10/postgres?sslmode=disable",
+        --     },
+        -- }),
         require("dbee.sources").EnvSource:new("DBEE_CONNECTIONS"),
         require("dbee.sources").FileSource:new(vim.fn.stdpath("cache") .. "/dbee/persistence.json"),
         -- require("dbee.sources").FileSource:new(vim.fn.getcwd() .. "/.sqllsrc.json"),
