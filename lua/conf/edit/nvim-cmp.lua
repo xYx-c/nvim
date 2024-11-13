7d74e24751e1d3dcea14a3e42cebd46df3498dde
x <xYx.c@outlook.com>
2024-11-13 17:55:58 +0800

    ..

--- lua/conf/edit/nvim-cmp.lua
+++ lua/conf/edit/nvim-cmp.lua
@@ -105,8 +105,6 @@ cmp.setup({
         -- 下一个
         ["<C-n>"] = cmp.mapping.select_next_item(),
         ["<down>"] = cmp.mapping.select_next_item(),
-        -- 选择补全
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
         -- 文档翻页
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
@@ -127,22 +125,34 @@ cmp.setup({
                 end
             end
         }),
+        -- 选择补全
+        ['<CR>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                if luasnip.expandable() then
+                    luasnip.expand()
+                else
+                    cmp.confirm({ select = true })
+                end
+            else
+                fallback()
+            end
+        end),
+
         ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
-                -- elseif luasnip.expand_or_locally_jumpable() then
-                --     luasnip.expand_or_jump()
-                -- elseif has_words_before() then
-                --     cmp.complete({})
+            elseif luasnip.locally_jumpable(1) then
+                luasnip.jump(1)
             else
                 fallback()
             end
         end, { "i", "s" }),
+
         ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_prev_item()
-                -- elseif luasnip.jumpable(-1) then
-                --     luasnip.jump(-1)
+            elseif luasnip.locally_jumpable(-1) then
+                luasnip.jump(-1)
             else
                 fallback()
             end
@@ -156,7 +166,7 @@ vim.keymap.set({ "i", "s" }, "<C-j>", function()
     end
 end, { silent = true })
 
-vim.keymap.set({ "i",  "s" }, "<C-k>", function()
+vim.keymap.set({ "i", "s" }, "<C-k>", function()
     if luasnip.jumpable(-1) then
         luasnip.jump(-1)
     end
